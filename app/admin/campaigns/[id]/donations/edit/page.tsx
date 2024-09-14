325ad5e5d1a6dbb5a7fa9d9bea3ae2eff0e2ee27
HasanNaaz <nazmul.hasan.naaz05@gmail.com>
2024-09-14 11:12:01 +0600

    Admin cp 1

--- app/admin/campaigns/[id]/donations/edit/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Page() { 
-  return (
-    <main>
-      <p>Editing Stocks</p>
-    </main>
-  )
-}
--- app/admin/campaigns/[id]/stocks/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Page() { 
-  return (
-    <main>
-      <p>Stocks</p>
-    </main>
-  )
-}

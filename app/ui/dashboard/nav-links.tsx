8a8088b65d4c48e5b27d568a6aeb487a6ab5b4e2
zaidrahmann <zaidrehman3103@gmail.com>
2024-09-14 14:10:27 +0600

    dashboard

---
+++ app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import {
+  ShieldCheckIcon,
+  UserGroupIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const links = [
+  { name: 'Admin Dashboard', href: '/admin', icon: ShieldCheckIcon },
+  {
+    name: 'Campaigns',
+    href: '/admin/campaigns',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Members', href: '/admin/members', icon: UserGroupIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 p-3 text-sm font-medium hover:bg-gray-100 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-gray-100 font-extrabold': pathname === link.href,
+              },
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}

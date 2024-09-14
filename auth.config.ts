8834c57d2738d04e8a18608ea3ff43cf7de20feb
HasanNaaz <nazmul.hasan.naaz05@gmail.com>
2024-09-14 10:47:59 +0600

    Authentication

---
+++ auth.config.ts
@@ -0,0 +1,29 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnAdminDashboard = nextUrl.pathname.startsWith('/admin');
+      const isOnUserDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnAdminDashboard) {
+        if (isLoggedIn) {
+          return true;
+        }
+        return false; // Redirect unauthenticated users to login page
+      } else if (isOnUserDashboard) { 
+        if (isLoggedIn) {
+          return true;
+        }
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/admin', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
